aff98d37d5d83e9a29846e11d5278f53d2c82a44
Srajan Kharvi <srajankharvi.dev@gmail.com>
2026-06-14 15:19:05 +0530

    facbook profile added

--- src/App.jsx
+++ src/App.jsx
@@ -498,6 +498,7 @@ function Contact() {
     { label: "GitHub", href: "https://github.com/srajankharvi", icon: <GitHubIcon /> },
     { label: "Instagram", href: "https://www.instagram.com/saju_kharvi_99/", icon: <InstagramIcon /> },
     { label: "LinkedIn", href: "https://www.linkedin.com/in/srajan-kharvi-6aba9a3b8/", icon: <LinkedInIcon /> },
+    { label: "Facebook", href: "https://www.facebook.com/people/Srajan-Kharvi/pfbid02gZrKaV9gbyt9rS636oz81M2YSfajPrKLctaDBGeLvaZCseZbFmZnSnWEptQpXuTrl/", icon: <FacebookIcon /> },
   ];
 
   return (
@@ -896,6 +897,14 @@ function EmailIcon() {
   return (
     <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current" aria-hidden="true">
       <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
+    </svg>
+  );
+}
+
+function FacebookIcon() {
+  return (
+    <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current" aria-hidden="true">
+      <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
     </svg>
   );
 }
